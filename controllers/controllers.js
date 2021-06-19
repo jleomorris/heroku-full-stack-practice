0e6cd32ab48a8992aa2b738c5adcda825c61cba2
jleomorris <james-morris2010@hotmail.co.uk>
2021-06-19 01:52:04 +0100

    Add back end config var test

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,16 @@
 const saySomething = (req, res, next) => {
-  res.status(200).json({
-    body: 'Hello from the server!',
-  });
+  if (
+    process.env.NODE_ENV === 'production' ||
+    process.env.NODE_ENV === 'staging'
+  ) {
+    res.status(200).json({
+      body: 'process.env.REACT_APP_FAKE_TEST_KEY',
+    });
+  } else {
+    res.status(200).json({
+      body: 'Hello from the server!',
+    });
+  }
 };
 
 module.exports.saySomething = saySomething;
